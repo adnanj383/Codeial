0079f5d7e424bee59a6e71054ec9f502a96b0377
adnanj383 <adnanj383@gmail.com>
2022-01-15 00:47:11 +0530

    Added users controller

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ const homeController = require('../controllers/home_controller')
 console.log('router loaded');
 
 router.get('/',homeController.home);
-// ye hamne likha tha -> router.get('/',homeController.home);
+router.use('/users', require('./users'));
+// what i did for post page -> router.use('/posts',require('./posts'));
+//router.use('/posts', require('./posts'));
+//ye hamne likha tha -> router.get('/',homeController.home);
+//for any further routes, access from here
+//router.use('/routerName', require('./routerFile));
 
 module.exports = router;
