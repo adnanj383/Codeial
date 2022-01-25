e8dba1e22d54e12635cf9f74195604e498a5bc84
adnanj383 <adnanj383@gmail.com>
2022-01-26 01:20:20 +0530

    Started Authentication and created sign up page and stored sign up details in database

--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,13 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+//Adding route for sign in page
+router.get('/sign-in', usersController.signIn);
+
+//Adding route for sign up page
+router.get('/sign-up', usersController.signUp);
+
+router.post('/create', usersController.create);
 
 module.exports = router;
 
-
-
-/*what I wrote for posts.js
-const express = require('express');
-const router = express.Router();
-
-
-const postsController = require('../controllers/posts_controller');
-
-router.get('/posts', postController.posts);
-
-
-
-
-module.exports = router;
-*/
