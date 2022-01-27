cee0695d2e1394fe7a596177c23ca6516d7c6827
adnanj383 <adnanj383@gmail.com>
2022-01-28 00:29:41 +0530

    Manual Authentication has been done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,58 +1,82 @@
-const User = require('../models/user');  
+const User = require('../models/user');
 
-module.exports.profile = function(req,res){
-    return res.render('users',{
-        title: "Users"
-    });
+
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
 
-//render the sign up page
-module.exports.signUp = function(req,res){
-    return res.render('user_sign_up',{
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
 }
 
 
-//render the sign in page
-module.exports.signIn = function(req,res){
-    return res.render('user_sign_in',{
-        title: "Codeial | Sign in"
+// render the sign in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
     })
 }
 
-//get the sing up data
-
+// get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
 
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){
-            console.log('error in finding user in signing up'); return}
-            
+        if(err){console.log('error in finding user in signing up'); return}
+
         if (!user){
             User.create(req.body, function(err, user){
-                if(err){
-                    console.log('error in creating user while signing up'); return}
-                    
-                return res.redirect('/users/sign-in');
+                if(err){console.log('error in creating user while signing up'); return}
 
+                return res.redirect('/users/sign-in');
             })
         }else{
             return res.redirect('back');
         }
-    })
+
+    });
 }
 
 
-//sign in and create a session for the user
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //TODO later
-}
 
-/*what I wrote for posts_controller.js
-/*module.exports.posts = function(req,res){
-    res.end('<h1>Post</h1>');
-}*/
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing in'); return}
+        // handle user found
+        if (user){
+
+            // handle password which doesn't match
+            if (user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+
+            return res.redirect('back');
+        }
+
+
+    });
+
+ 
+
+    
+
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
-//Adding route for sign in page
+
+router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
-//Adding route for sign up page
-router.get('/sign-up', usersController.signUp);
 
 router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
+
 
 module.exports = router;
-
